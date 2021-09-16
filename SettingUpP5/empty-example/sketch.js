de2c6ebbbf607818d0d1af9560b64147c2fce3c6
Sarah <sa4913@nyu.edu>
2021-09-16 16:40:33 +0800

    successful connection to the p5 library

--- SettingUpP5/empty-example/sketch.js
+++ SettingUpP5/empty-example/sketch.js
@@ -1,8 +1,10 @@
 function setup() {
   // put setup code here
   ellipse(50,50,100,100);
+  createCanvas(1280,960);
 }
 
 function draw() {
   // put drawing code here
+  ellipse(mouseX, mouseY, 100, 100);
 }
